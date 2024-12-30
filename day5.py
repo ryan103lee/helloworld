d8fc312ced0158b8a9b631451438b6dd90d26c0c
ryan103lee <85479066+ryan103lee@users.noreply.github.com>
2024-12-30 23:12:21 +0800

    practising file

---
+++ day5.py
@@ -0,0 +1,43 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header('st.write')
+
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+st.write('1234')
+
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+
+df2 = pd.DataFrame(
+     np.random.randn(100, 5),
+     columns=['a', 'b', 'c','d',
+              'e'])
+st.write(df2)
+
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
+
+
+
---
+++ st_header.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("This is a header with a divider", divider="gray")
+st.header("These headers have rotating dividers", divider=True)
+st.header("One", divider=True)
+st.header("Two", divider=True)
+st.header("Three", divider=True)
+st.header("Four", divider=True)
+st.subheader("One", divider=True)
+st.subheader("Two", divider=True)
+st.subheader("Three", divider=True)
+st.subheader("Four", divider=True)
+
+#import streamlit as st
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language="python")
+
+
+import streamlit as st
+code = '''Is it a crown or boat?
+                        ii
+                      iiiiii
+WWw                 .iiiiiiii.                ...:
+ WWWWWWw          .iiiiiiiiiiii.         ........
+  WWWWWWWWWWw    iiiiiiiiiiiiiiii    ...........
+   WWWWWWWWWWWWWWwiiiiiiiiiiiiiiiii............
+    WWWWWWWWWWWWWWWWWWwiiiiiiiiiiiiii.........
+     WWWWWWWWWWWWWWWWWWWWWWwiiiiiiiiii.......
+      WWWWWWWWWWWWWWWWWWWWWWWWWWwiiiiiii....
+       WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWwiiii.
+          -MMMWWWWWWWWWWWWWWWWWWWWWWMMM-
+'''
+st.code(code, language=None)
+
